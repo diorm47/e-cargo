1fd4dc1730bd691a64777fa78eb8430990a0a44f
dior <dior47m@gmail.com>
2023-04-19 20:06:17 +0500

    deploy

---
+++ index.js
@@ -0,0 +1,9 @@
+const modal_item = document.getElementById("modal_item");
+
+function modal_toggle() {
+  modal_item.style.display = "flex";
+}
+
+function exit_button() {
+  modal_item.style.display = "none";
+}
